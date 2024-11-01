b62c61651c6825c0fbd354fc97e40cef630b9c1a
23516115 <kouta_haginoya@icloud.com>
2024-11-01 19:27:28 +0900

    Create 課題3.c

---
+++ 課題3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void)
+{
+    printf("long long int absolute maimum:%lld\n", ULLONG_MAX);
+    printf("long long int maximum:%lld\n", LLONG_MAX);
+    printf("long long int minimum:%lld\n", LLONG_MIN);
+    
+    return 0;
+}
